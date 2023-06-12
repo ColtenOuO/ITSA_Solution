ac5856a9c62201e92ebbbde1b228a8a8b54ee54c
ColtenOuO <jun930436@gmail.com>
2023-06-12 15:35:28 +0800

    wrong answer in problem3.

---
+++ 2023_05/problem3/solution.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <iomanip>
+#define int long long
+
+using namespace std;
+
+char id[21];
+int w[21];
+
+void init()
+{
+    for(int i=0;i<26;i++) w[i] = -1;
+    return;
+}
+double solve(double num)
+{
+    num *= 1000;
+    if( (int)num % 10 >= 5 ) num /= 10 , num += 1 , num /= 100;
+    else num /= 1000;
+
+    return num;
+}
+signed main(void)
+{
+    int n,sum = 0;
+    cin >> n;
+    
+    init();
+    int c = 0;
+    for(int i=0;i<n;i++) cin >> id[i] >> w[ id[i] - 'A' ] , sum += w[ id[i] - 'A' ];
+    for(int i=0;i<26;i++)
+    {
+        if( w[i] != -1 )
+        {
+            double ans = (double)w[i] / (double)sum;
+            
+            ans = solve(ans);
+            ans *= 100;
+            ans = (int)ans;
+            ans /= 100;
+            
+            if( c != 0 ) cout << "\n";
+            c = 1;
+
+            cout << fixed << setprecision(2) << (char)('A'+i) << "(" << ans  << ")";
+        }
+    }
+
+    return 0;
+}
